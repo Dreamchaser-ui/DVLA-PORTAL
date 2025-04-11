7ed4487a9d340e2793c317800aa552f3f20cb5b3
Nana Yirenkyi <acheampongyirenkyinana071@gmail.com>
2025-04-11 00:39:06 +0000

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  if (username === "Lord Ares" && password === "Dreamchaser@25") {
+    window.location.href = "admin.html";
+  } else {
+    alert("Invalid credentials!");
+  }
+});
